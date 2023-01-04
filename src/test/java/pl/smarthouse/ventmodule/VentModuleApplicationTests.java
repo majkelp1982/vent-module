19e6f10dd71a0a2e02812523256ef5f2090812d1
majkelp1982 <s.pomazanka@gmail.com>
2023-01-04 23:12:14 +0100

    enable test

--- src/test/java/pl/smarthouse/ventmodule/VentModuleApplicationTests.java
+++ src/test/java/pl/smarthouse/ventmodule/VentModuleApplicationTests.java
@@ -1,10 +1,11 @@
 package pl.smarthouse.ventmodule;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VentModuleApplicationTests {
 
-  //  @Test
-  //  void contextLoads() {}
+  @Test
+  void contextLoads() {}
 }
