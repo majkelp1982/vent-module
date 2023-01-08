b9298fe559b0e1fc22cfecf5f65a1f50598485d3
majkelp1982 <s.pomazanka@gmail.com>
2023-01-08 21:48:57 +0100

    rename actors

--- src/main/java/pl/smarthouse/ventmodule/schedulers/EventScheduler.java
+++ src/main/java/pl/smarthouse/ventmodule/schedulers/EventScheduler.java
@@ -16,19 +16,20 @@ public class EventScheduler {
   private final ModuleService moduleService;
   private final ModuleConfig moduleConfig;
 
-  @Scheduled(fixedDelay = 5000)
+  @Scheduled(fixedDelay = 10000)
   public void eventScheduler() {
     moduleService.exchange().subscribe();
-//    final BME280 bme280Intake = (BME280) moduleConfig.getActor(ModuleConfig.BME280_INTAKE);
-//    bme280Intake.setCommandSet(new BME280CommandSet(BME280CommandType.READ));
+    //    final BME280 bme280Intake = (BME280) moduleConfig.getActor(ModuleConfig.BME280_INTAKE);
+    //    bme280Intake.setCommandSet(new BME280CommandSet(BME280CommandType.READ));
 
-//    final BME280 bme280Outlet = (BME280) moduleConfig.getActor(ModuleConfig.BME280_OUTLET);
-//    bme280Outlet.setCommandSet(new BME280CommandSet(BME280CommandType.READ));
+    //    final BME280 bme280Outlet = (BME280) moduleConfig.getActor(ModuleConfig.BME280_OUTLET);
+    //    bme280Outlet.setCommandSet(new BME280CommandSet(BME280CommandType.READ));
 
-//    final BME280 bme280FreshAir = (BME280) moduleConfig.getActor(ModuleConfig.BME280_FRESH_AIR);
-//    bme280FreshAir.setCommandSet(new BME280CommandSet(BME280CommandType.READ));
+    //    final BME280 bme280FreshAir = (BME280)
+    // moduleConfig.getActor(ModuleConfig.BME280_FRESH_AIR);
+    //    bme280FreshAir.setCommandSet(new BME280CommandSet(BME280CommandType.READ));
 
-//    final BME280 bme280UsedAir = (BME280) moduleConfig.getActor(ModuleConfig.BME280_USED_AIR);
-//    bme280UsedAir.setCommandSet(new BME280CommandSet(BME280CommandType.READ));
+    //    final BME280 bme280UsedAir = (BME280) moduleConfig.getActor(ModuleConfig.BME280_USED_AIR);
+    //    bme280UsedAir.setCommandSet(new BME280CommandSet(BME280CommandType.READ));
   }
 }
