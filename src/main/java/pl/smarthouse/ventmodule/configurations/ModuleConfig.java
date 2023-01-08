db1b060d5f203d02b585e7e350d2a2478921533f
majkelp1982 <s.pomazanka@gmail.com>
2023-01-08 21:48:41 +0100

    rename actors

--- src/main/java/pl/smarthouse/ventmodule/configurations/ModuleConfig.java
+++ src/main/java/pl/smarthouse/ventmodule/configurations/ModuleConfig.java
@@ -17,24 +17,26 @@ import javax.annotation.PostConstruct;
 public class ModuleConfig {
   // Actors
   // Temp, humid, pressure sensors
-  public static final String BME280_INTAKE = "bme280_intake";
+  public static final String BME280_INLET = "bme280_inlet";
   public static final String BME280_OUTLET = "bme280_outlet";
   public static final String BME280_FRESH_AIR = "bme280_fresh_air";
   public static final String BME280_USED_AIR = "bme280_used_air";
   // Fan PWM controllers
+  // Generic
   public static final int FAN_FREQUENCY = 1000;
   public static final int FAN_RESOLUTION = 8;
-  public static final String FAN_INTAKE = "fan_intake";
-  public static final int FAN_INTAKE_CHANNEL = 2;
-  public static final int FAN_INTAKE_PIN = 33;
-
+  // Fan intake
+  public static final String FAN_INLET = "fan_inlet";
+  public static final int FAN_INLET_CHANNEL = 2;
+  public static final int FAN_INLET_PIN = 33;
+  // Fan outlet
   public static final String FAN_OUTLET = "fan_outlet";
   public static final int FAN_OUTLET_CHANNEL = 3;
   public static final int FAN_OUTLET_PIN = 2;
 
   // Module specific
-  private static final String FIRMWARE = "20230104.17";
-  private static final String VERSION = "20230103.22";
+  private static final String FIRMWARE = "20230107.00";
+  private static final String VERSION = "20230107.23";
   private static final String MAC_ADDRESS = "3C:71:BF:4D:6A:40";
   private static final String MODULE_TYPE = "VENTILATION";
   private final pl.smarthouse.smartmodule.model.configuration.Configuration configuration;
@@ -57,16 +59,24 @@ public class ModuleConfig {
     final ActorMap actorMap = new ActorMap();
 
     // BME280 sensors
-    actorMap.putActor(new BME280(BME280_INTAKE, 13));
+    actorMap.putActor(new BME280(BME280_INLET, 13));
     actorMap.putActor(new BME280(BME280_OUTLET, 14));
     actorMap.putActor(new BME280(BME280_FRESH_AIR, 27));
     actorMap.putActor(new BME280(BME280_USED_AIR, 26));
 
     // PWM actors
     actorMap.putActor(
-        new PWM(FAN_INTAKE, FAN_INTAKE_CHANNEL, FAN_FREQUENCY, FAN_RESOLUTION, FAN_INTAKE_PIN));
+        new PWM(
+            FAN_INLET, FAN_INLET_CHANNEL, FAN_FREQUENCY, FAN_RESOLUTION, FAN_INLET_PIN, 0, true));
     actorMap.putActor(
-        new PWM(FAN_OUTLET, FAN_OUTLET_CHANNEL, FAN_FREQUENCY, FAN_RESOLUTION, FAN_OUTLET_PIN));
+        new PWM(
+            FAN_OUTLET,
+            FAN_OUTLET_CHANNEL,
+            FAN_FREQUENCY,
+            FAN_RESOLUTION,
+            FAN_OUTLET_PIN,
+            0,
+            true));
     return actorMap;
   }
 
