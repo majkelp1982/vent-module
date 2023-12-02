9fac1d351f0e2527e50e7b75e17c926106d07176
majkelp1982 <s.pomazanka@gmail.com>
2023-12-02 09:23:38 +0100

    !!! 20231202.08 !!!

--- src/main/java/pl/smarthouse/ventmodule/properties/Esp32ModuleProperties.java
+++ src/main/java/pl/smarthouse/ventmodule/properties/Esp32ModuleProperties.java
@@ -2,8 +2,8 @@ package pl.smarthouse.ventmodule.properties;
 
 public class Esp32ModuleProperties {
   // Module specific
-  public static final String FIRMWARE = "20231116.19";
-  public static final String VERSION = "20230318.12";
+  public static final String FIRMWARE = "20231202.08";
+  public static final String VERSION = "20231202.09";
   public static final String MAC_ADDRESS = "3C:71:BF:4D:6A:40";
   public static final String MODULE_TYPE = "VENTILATION";
 }
